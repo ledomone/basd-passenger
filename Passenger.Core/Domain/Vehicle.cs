322bb005aa04ca124ba2dcf18199bddfb258573f
ledomone <ledom.git@gmail.com>
2018-02-17 00:08:44 +0100

    Vehicle

---
+++ Passenger.Core/Domain/Vehicle.cs
@@ -0,0 +1,9 @@
+namespace Passenger.Core
+{
+    public class Vehicle
+    {
+        public string Brand { get; protected set; }
+        public string Name { get; protected set; }
+        public int Seats { get; protected set; }
+    }
+}
