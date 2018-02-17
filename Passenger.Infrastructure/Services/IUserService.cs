ce343d58487a1151205a8be9209b4be26210432d
ledomone <ledom.git@gmail.com>
2018-02-17 14:43:41 +0100

    Infrastructure: UserService

---
+++ Passenger.Infrastructure/Services/IUserService.cs
@@ -0,0 +1,7 @@
+namespace Passenger.Infrastructure.Services
+{
+    public interface IUserService
+    {
+         void Register(string email, string username, string password);
+    }
+}
---
+++ Passenger.Infrastructure/Services/UserService.cs
@@ -0,0 +1,26 @@
+using System;
+using Passenger.Core.Domain;
+using Passenger.Core.Repositories;
+
+namespace Passenger.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public void Register(string email, string username, string password)
+        {
+            var user = _userRepository.Get(email);
+            if (user != null)
+            {
+                throw new Exception($"User with email: '{email}' already exists.");
+            }
+            var salt = Guid.NewGuid().ToString("N");
+            user = new User(email, username, password, salt);
+            _userRepository.Add(user);
+        }
+    }
+}
