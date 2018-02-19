1077b96052bc2ccc325b7c1155596183d77563d6
ledomone <ledom.git@gmail.com>
2018-02-19 23:16:19 +0100

    Driver model - remove Id

--- Passenger.Core/Domain/Driver.cs
+++ Passenger.Core/Domain/Driver.cs
@@ -5,7 +5,6 @@ namespace Passenger.Core.Domain
 {
     public class Driver
     {
-        public Guid Id { get; protected set; }
         public Guid UserId { get; protected set; }
         public Vehicle Vehicle { get; protected set; }
         protected IEnumerable<Route> Routes { get; private set; }
