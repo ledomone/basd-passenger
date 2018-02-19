92d0228a921be49b884c99a54a22fba10c01985e
ledomone <ledom.git@gmail.com>
2018-02-19 23:24:52 +0100

    Vehicle - private constructor and static Create method

--- Passenger.Core/Domain/Vehicle.cs
+++ Passenger.Core/Domain/Vehicle.cs
@@ -5,5 +5,15 @@ namespace Passenger.Core
         public string Brand { get; protected set; }
         public string Name { get; protected set; }
         public int Seats { get; protected set; }
+
+        private Vehicle(string brand, string name, int seats)
+        {
+            Brand = brand;
+            Name = name;
+            Seats = seats;
+        }
+
+        public static Vehicle Create(string brand, string name, int seats)
+            => new Vehicle(brand, name, seats);
     }
 }
