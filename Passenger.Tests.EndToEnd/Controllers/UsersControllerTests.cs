2fc927d31c0fb2dcdc937716ed12eae6e07fe54d
ledomone <ledom.git@gmail.com>
2018-05-08 01:01:09 +0200

    Api test

---
+++ Passenger.Tests.EndToEnd/Controllers/UsersControllerTests.cs
@@ -0,0 +1,37 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Passenger.Api;
+using Xunit;
+
+namespace Passenger.Tests.EndToEnd.Controllers
+{
+    public class UsersControllerTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public UsersControllerTests()
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnHelloWorld()
+        {
+            // Act
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal("Hello World!",
+                responseString);
+        }
+    }
+}
